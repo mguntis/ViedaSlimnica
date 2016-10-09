47668cee79fb254eaa664a6e4a2bf0c3bb9c59a4
MGuntis <MGuntis@DESKTOP-RB57U0B>
2016-10-09 16:17:52 +0300

    Created template

---
+++ ViedaSlimnicaProject/ViedaSlimnicaProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViedaSlimnicaProject.Startup))]
+namespace ViedaSlimnicaProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
