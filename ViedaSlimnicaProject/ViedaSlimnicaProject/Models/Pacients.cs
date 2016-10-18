04379ecde37707946ffc00eb33edcb8faaa2de73
MGuntis <MGuntis@DESKTOP-RB57U0B>
2016-10-18 14:38:52 +0300

    Vēl mazi dizaina uzlabojumi

--- ViedaSlimnicaProject/ViedaSlimnicaProject/Models/Pacients.cs
+++ ViedaSlimnicaProject/ViedaSlimnicaProject/Models/Pacients.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace ViedaSlimnicaProject.Models
 {
@@ -11,16 +12,23 @@ namespace ViedaSlimnicaProject.Models
         [Key]
         public int PacientaID { get; set; }
         [StringLength(50)]
+        [DisplayName("Vārds")]
         public string Vards { get; set; }
         [StringLength(50)]
+        [DisplayName("Uzvārds")]
         public string Uzvards { get; set; }
         [StringLength(12)]
+        [DisplayName("Personas kods")]
         public string PersKods { get; set; }
         [StringLength(50)]
+        [DisplayName("Simptomi")]
         public string Simptomi { get; set; }
         [StringLength(50)]
+        [DisplayName("Nodaļa")]
         public string Nodala { get; set; } //Tas arī ir definēts klassē palātā bet nevar izmantot to pašu? 
+        [DisplayName("Pālātas Nr.")]
         public int PalatasID { get; set; }
+        [DisplayName("Ierašanās datums")]
         public DateTime IerasanasDatums { get; set; }
 
         //Test
