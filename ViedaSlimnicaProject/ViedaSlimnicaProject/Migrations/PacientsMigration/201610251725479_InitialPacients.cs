97269a917e669ef6c5e364c7246ad5c63ce31aa0
sragia <a.bedeicis@gmail.com>
2016-10-25 20:17:11 +0300

    .

---
+++ ViedaSlimnicaProject/ViedaSlimnicaProject/Migrations/PacientsMigration/201610251725479_InitialPacients.cs
@@ -0,0 +1,16 @@
+namespace ViedaSlimnicaProject.Migrations.PacientsMigration
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialPacients : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ ViedaSlimnicaProject/ViedaSlimnicaProject/Migrations/PalataMigration/201610251726529_InitialPalata.cs
@@ -0,0 +1,16 @@
+namespace ViedaSlimnicaProject.Migrations.PalataMigration
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialPalata : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
