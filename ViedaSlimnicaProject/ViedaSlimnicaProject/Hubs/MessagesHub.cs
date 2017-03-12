54b00c2d6f200264798a2e9db261a8ff10c34411
sragia <a.bedeicis@gmail.com>
2017-03-12 15:00:49 +0200

    Neizlasītajiem ziņojumiem rāda tekstu "Jauna!", nospiežot uz ziņas, tā tiek atzīmēta kā izlasīta

---
+++ ViedaSlimnicaProject/ViedaSlimnicaProject/Hubs/MessagesHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ViedaSlimnicaProject.Hubs
+{
+    public class MessagesHub : Hub
+    {
+        private SmartHospitalDatabaseContext db = new SmartHospitalDatabaseContext();
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void MarkMessage(int? msgID)
+        {
+            if (msgID != null)
+            {
+                db.Zinojumi.Find(msgID).isRead = true;
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- ViedaSlimnicaProject/ViedaSlimnicaProject/Scripts/Messages.js
+++ ViedaSlimnicaProject/ViedaSlimnicaProject/Scripts/Messages.js
@@ -1,25 +1,33 @@
-﻿function addID(msgID, mode) { 
-    var id = mode + 'modalMsgID';
-    var modal = document.getElementById(id)
-    modal.value = msgID;
-}
-function editReply(msgID, mode, msghtmlID) {
-    if (mode === "edit") {
-        // labot
-        document.getElementById('editreplyTitle').innerHTML = "Labot ziņu";
-        document.getElementById('editreplyMode').value = 'edit';
-        document.getElementById('editMsgID').value = msgID;
-        document.getElementById('editreplySubject').value = document.getElementById('msgSubject' + msghtmlID).innerHTML;
-        $('#editreplyText').val(document.getElementById('message' + msghtmlID).innerHTML);
-    }
-    else {
-        // atbildēt
-        var msgTo = document.getElementById('msgAuthor' + msghtmlID).innerHTML;
-        document.getElementById('editreplyTitle').innerHTML = "Atbildēt " + msgTo;
-        document.getElementById('editreplyMode').value = 'reply';
-        document.getElementById('editMsgID').value = msgID;
-        document.getElementById('editreplySubject').value = 'RE: ' + document.getElementById('msgSubject' + msghtmlID).innerHTML;
-        $('#editreplyText').val(""); 
+﻿var chat = $.connection.messagesHub;
 
+    function addID(msgID, mode) {
+        var id = mode + 'modalMsgID';
+        var modal = document.getElementById(id)
+        modal.value = msgID;
     }
-}
+    function editReply(msgID, mode, msghtmlID) {
+        if (mode === "edit") {
+            // labot
+            document.getElementById('editreplyTitle').innerHTML = "Labot ziņu";
+            document.getElementById('editreplyMode').value = 'edit';
+            document.getElementById('editMsgID').value = msgID;
+            document.getElementById('editreplySubject').value = document.getElementById('msgSubject' + msghtmlID).innerHTML;
+            $('#editreplyText').val(document.getElementById('message' + msghtmlID).innerHTML);
+        }
+        else {
+            // atbildēt
+            var msgTo = document.getElementById('msgAuthor' + msghtmlID).innerHTML;
+            document.getElementById('editreplyTitle').innerHTML = "Atbildēt " + msgTo;
+            document.getElementById('editreplyMode').value = 'reply';
+            document.getElementById('editMsgID').value = msgID;
+            document.getElementById('editreplySubject').value = 'RE: ' + document.getElementById('msgSubject' + msghtmlID).innerHTML;
+            $('#editreplyText').val("");
+
+        }
+    }
+    function updateMessage(msgID, htmlID) {
+        $.connection.hub.start().done(function () {
+            chat.server.markMessage(msgID);
+            $('#msgNew' + htmlID).html('');
+        });
+    }
